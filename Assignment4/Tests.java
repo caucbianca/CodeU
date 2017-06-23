c6a0a92e833542e57dd1cb965b47757a909f5e4b
caucbianca <cauc.bianca@yahoo.ro>
2017-06-23 21:16:34 +0300

    Update Tests.java

--- Assignment4/Tests.java
+++ Assignment4/Tests.java
@@ -9,9 +9,9 @@ public class Tests {
 	@Test
 	public void test1() {
 		boolean[][] grid = { { false, true, false, true }, 
-							 { true, true, false, false },
-							 { false, false, true, false }, 
-							 { false, false, true, false } };
+				     { true, true, false, false },
+				     { false, false, true, false }, 
+				     { false, false, true, false } };
 
 		Assert.assertEquals(3, CountingIslands.getNumberOfIslands(grid));
 	}
@@ -19,20 +19,21 @@ public class Tests {
 	@Test
 	public void test2() {
 		boolean[][] grid = { { true, false, false, false, true, true, false, false, false, false },
-							 { true, true, false, true, false, true, false, true, true, true },
-							 { false, true, true, false, false, true, true, false, false, true },
-							 { true, false, false, false, true, false, false, true, false, true },
-							 { true, true, true, false, true, true, true, true, false, true },
-							 { false, true, true, false, false, true, true, false, false, true },
-							 { false, false, false, true, true, true, false, true, true, true },
-							 { true, true, true, false, false, false, true, false, false, false } };
+				     { true, true, false, true, false, true, false, true, true, true },
+				     { false, true, true, false, false, true, true, false, false, true },
+				     { true, false, false, false, true, false, false, true, false, true },
+				     { true, true, true, false, true, true, true, true, false, true },
+				     { false, true, true, false, false, true, true, false, false, true },
+				     { false, false, false, true, true, true, false, true, true, true },
+				     { true, true, true, false, false, false, true, false, false, false } };
+		
 		Assert.assertEquals(8, CountingIslands.getNumberOfIslands(grid));
 	}
 	
 	@Test
 	public void test3() {
 		boolean[][] grid = { { true, true },
-							 { true, true } };
+				     { true, true } };
 
 		Assert.assertEquals(1, CountingIslands.getNumberOfIslands(grid));
 	}
